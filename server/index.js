f8a733fa3d8e9f16ada288176837fe4ba79e4a3f
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-11-22 03:10:32 -0800

    implement link shortening API with Express and MongoDB

---
+++ server/index.js
@@ -0,0 +1,70 @@
+import  express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+import Link from './models/Link.js';
+
+const app = express();
+app.use(express.json());
+
+const connectDB =async () => {
+    const conn =await mongoose.connect(process.env.MONGODB_URI);
+
+    if (conn) {
+        console.log(`MongoDB connected successfully`);
+    }
+};
+connectDB();
+
+app.post("/link",async (req,res) =>{
+ const {url,slug}=req.body;
+
+ const randomSlug = Math.random().toString(36).substring(2,7);
+ 
+const link =new Link({
+   url:url,
+   slug:slug || randomSlug
+})
+try{
+    const savedLink =await link.save();
+     return res.json({
+       success:true,
+       data: {
+       shortLink:`${process.env.BASE_URL}/${savedLink.slug}`
+       },
+       message:"Link saved successfully"
+    })
+}
+catch (err){
+return res.json({
+    success:false,
+    message:err.message
+})
+}
+})
+
+app.get("/:slug", async (req,res) => {
+    const {slug} = req.params;
+
+    const link =await Link.findOne({slug:slug});
+
+
+    await Link.updateOne({slug:slug},{$set: {
+        clicks:link.clicks + 1
+    }})
+    if(!link){
+      return res.json({
+        success:false,
+        message:"Link not found"
+      })  
+    }
+    res.redirect(link.url);
+})
+
+
+
+const PORT = process.env.PORT ||5000;
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ server/models/Link.js
@@ -0,0 +1,24 @@
+import { Schema,model } from "mongoose";
+
+const linkSchema =new Schema({
+    url : {
+        type :String,
+        required: true,
+    },
+    slug:{
+        type :String,
+        required: true,
+        unique:true,
+    },
+clicks:{
+    type:Number,
+    required:true,
+    default:0,
+},
+}, {
+    timestamps:true,
+});
+
+const Link = model('Link',linkSchema);
+
+export default Link;
