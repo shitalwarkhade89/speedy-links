ebbc311a535e38345cb06a1fc9cd63b437abef58
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-11-26 04:26:08 -0800

    add path

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import Link from './models/Link.js';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
+const __dirname = path.resolve();
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
@@ -89,3 +91,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+  }
